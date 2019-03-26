38fa038a9a4e21bf731432f0ab1439eea6bc5258
rugosh <kuhn_tobias@gmx.net>
2019-03-26 10:37:55 +0100

    changing the search text in the javascript theme file for german

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -80,7 +80,7 @@
 
   $(window).load(function() {
     $( "#quick-search form" ).css('margin-right', $( "#s2id_project_quick_jump_box" ).width() + 60);
-    $( 'input[name$="q"]' ).attr( 'placeholder','Enter Search Text' );
+    $( 'input[name$="q"]' ).attr( 'placeholder','Suchtext hier eingeben' );
     if (activeStaticSidebar) {
       $( "#wrapper3" ).css( "margin-left", "215px" );
       $( "#quick-search" ).css( "left", "200px" );
